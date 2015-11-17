7b88501b31ed713d256cd298fa2efce61a42d50e
KG <kerezsigergo@hotmail.com>
2015-11-17 20:19:34 +0100

    E-mail check

--- loginSystem.js
+++ loginSystem.js
@@ -1,15 +1,29 @@
 if (Meteor.isClient) {
   // counter starts at 0
+
+  function validateEmail(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+  }
+
+
   Template.register.events({
     'submit form': function(event,template){
       event.preventDefault();
       var emailVar = template.find('#email').value;
-      var passwordVar = template.find('#password').value; 
-      console.log('Register.');
-      Accounts.createUser({
-        email: emailVar,
-        password: passwordVar
-      })
+      var passwordVar = template.find('#password').value;
+      if (validateEmail(emailVar)){
+          console.log('Register.');
+          Accounts.createUser({
+          email: emailVar,
+          password: passwordVar
+        })
+      } else {
+        console.log('Invalid e-mail.');
+      }
+
+
+      
     }
   });
 
@@ -19,7 +33,7 @@ if (Meteor.isClient) {
       var emailVar = template.find('#login-email').value;
       var passwordVar = template.find('#login-password').value; 
       Meteor.loginWithPassword(emailVar, passwordVar);
-      console.log('Loged in.');
+      console.log('Logged in.');
     }
   });
 
