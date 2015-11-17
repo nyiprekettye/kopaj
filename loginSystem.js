9701dafe666851fd2bb57d8528dc41ef5d5b68ee
KG <kerezsigergo@hotmail.com>
2015-11-17 19:59:37 +0100

    initial

---
+++ loginSystem.js
@@ -0,0 +1,32 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Template.register.events({
+    'submit form': function(event,template){
+      event.preventDefault();
+      var emailVar = template.find('#email').value;
+      var passwordVar = template.find('#password').value; 
+      console.log('Form submitted.');
+      Accounts.createUser({
+        email: emailVar,
+        password: passwordVar
+      })
+    }
+  });
+
+  Template.login.events({
+    'submit form': function(event,template){
+      event.preventDefault();
+      var emailVar = template.find('#login-email').value;
+      var passwordVar = template.find('#login-password').value; 
+      Meteor.loginWithPassword(emailVar, passwordVar);
+      console.log('Loged in.');
+    }
+  });
+
+  Template.dashboard.events({
+    'click .logout': function(event){
+      event.preventDefault();
+      Meteor.logout();
+    }
+  })
+}
